beaeb73fbd50f6156764d53e41069d81da237c63
dmichaelides <48203524+dmichaelides@users.noreply.github.com>
2019-03-25 17:54:55 +0200

    Homework #1 assignment

---
+++ main.py
@@ -0,0 +1,31 @@
+"""
+HEADING - 'Python-is-Easy' Homework #1 (Variables)
+DESCRIPTION: 
+The main goal of this file created, is to get acquianted with variables in Python.  It is the first homework assigment in the
+'Python is Easy' course, from Pirple.
+
+The details below are attributes to a song, which have been created as variables in the file.
+P.S. This is not really my best song of all time.
+"""
+
+Title = "In the End" # This is the name of the song.
+Artist = "Linkin Park"
+Album = "Hybrid Theory"
+Genre = "Nu metal - Rap Rock"
+DurationInSeconds = 216 # This is the length of the song in seconds.
+DurationInMinutes = 3.6
+RecordedDate = 2000
+ReleaseDate = "October 9, 2001"
+Label = "Warner Bros." # The Record Label company who published the song. 
+WikipediaPage = "https://en.wikipedia.org/wiki/In_the_End" # Added the Wikipage as reference on where I got the informtion from.
+
+print(Title)
+print(Artist)
+print(Album)
+print(Genre)
+print(DurationInSeconds)
+print(DurationInMinutes)
+print(RecordedDate)
+print(ReleaseDate)
+print(Label)
+print(WikipediaPage)
